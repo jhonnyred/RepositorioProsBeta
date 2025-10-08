d9aca2c04c42fb2acb64179f218e83cc89c0b018
Jhonnyred <grodriguesgalli@gmail.com>
2025-10-08 22:54:42 +0000

    08/10

---
+++ aula9/src/Funcionario.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Funcionario{
+    // ATRIBUTOS
+    String nome;
+    ArrayList funcList = new Arraylist<Funcionario>();
+
+    // CONSTRUTOR
+    public Funcionario(String nome){
+        this.nome = nome;
+    }
+
+    // MÉTODOS
+    public void addFuncionario(Funcionario func){
+        funcList.add(func);
+    }
+
+    public void imprimir(){
+        System.out.println("Nome: " +this.nome);
+    }
+
+    public void imprimirSubordinados
+}
